fe04c2e85033ea26159b83e0a325ad86f70acb5f
Benjamin Onwenu <bonwenu@emich.edu>
2020-10-08 18:15:00 -0400

    Added Repository annotation and removed un-needed imports

--- account-service/src/main/java/com/chat/projects/repositories/ApplicationUserRepository.java
+++ account-service/src/main/java/com/chat/projects/repositories/ApplicationUserRepository.java
@@ -1,9 +1,11 @@
 package com.chat.projects.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chat.projects.models.ApplicationUser;
 
+@Repository
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
     ApplicationUser findByUsername(String username);
     ApplicationUser findByEmail(String email);
